9a480474005ed6b2dc59ddc0b3d524d80c07db18
Taiwo-Adeoluwa <omogunloyetaiwo5@gmail.com>
2026-06-10 19:54:01 +0100

    neww

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 require('dotenv').config();
+const PORT = process.env.PORT
 const express = require('express');
-const mongoose = require('mongoose')
-const http = require('http');
-const {Server} = require('socket.io')
+const app = express();
 const cors = require('cors');
+const swagger = require('./swagger');
+const swaggerUi = require('swagger-ui-express');
+const session = require('express-session')
 
-//Routers
+
 const vendorRouter = require('./routes/vendorRouter');
 const router = require('./routes/userRouter');
 const pricingRouter = require('./routes/pricingRouter');
@@ -14,41 +16,6 @@ const calendarRouter = require('./routes/calendarRouter');
 const bookingRouter = require('./routes/bookingRouter');
 const kycRouter = require('./routes/kycRouter')
 
-
-// const { initializeIO } = require('./controllers/messageController');
-
-const app = express();
-app.use(cors({origin: '*'}));
-const server = http.createServer(app);
-
-// ✅ Create IO instance first
-// const io = new Server(server, {
-//   cors: {
-//     origin: "*",
-//     methods: ["GET", "POST"],
-//   },
-  
-  // // ✅ Now initialize IO globally for the controller
-// initializeIO(io);
-// console.log("✅ initializeIO called in server.js")
-
-// Import modular Socket.IO logic
-// io.on("connection", (socket) => {
-//   console.log("User connected:", socket.id);
-
-//   // ⭐ Join room (correct way)
-//   socket.on("join_room", (roomId) => {
-//     socket.join(roomId);
-//     console.log("Joined room:", roomId);
-//   });
-
-
-//   socket.on("disconnect", () => {
-//     console.log("User disconnected:", socket.id);
-//   });
-// });
-
-//Rate limiter
 const rateLimit = require('express-rate-limit');
 
 const limiter = rateLimit({
@@ -61,12 +28,17 @@ const limiter = rateLimit({
 	// store: ... , // Redis, Memcached, etc. See below.
 });
 
-// ---- MIDDLEWARES ----\
 app.use(express.json());
+app.use(cors());
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swagger));
 
+app.use(session({
+  secret: 'dabest',
+  resave: false,
+  saveUninitialized: false
+ }))
 
-// ---- ROUTES ----
-app.use('/api/v1/user', router);
+ app.use('/api/v1/user', router);
 app.use('/api/v1/vendor', vendorRouter);
 app.use('/api/v1', pricingRouter);
 app.use('/api/v1/payment', paymentRouter);
@@ -74,49 +46,37 @@ app.use('/api/v1/schedule', calendarRouter);
 app.use('/api/v1/bookings', bookingRouter);
 app.use('/api/v1/kyc', kycRouter)
 
-// Swagger setup
-const swagger = require('./swagger');
-const swaggerUi = require('swagger-ui-express');
-
-// const options = { swaggerDefinition, apis:['/routes/*.js']};
-// const swaggerSpec = swaggerJSDoc(options);
-// app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swagger));
-
-// Root route
-app.get('/', (req, res) => {
-  res.send('Welcome to Feastsync Server!');
-});
-
-//Global error handler
- app.use((error,req, res, next) => {
-  if(error){
-  res.status(500).json({
-    message: error.message
-  });
-}
-next()
-});
-
-  app.use((err, req, res, next) => {
-  if (err instanceof multer.MulterError) {
-    return res.status(400).json({ message: err.message });
-  } else if (err) {
-    return res.status(400).json({ message: err.message });
-  }
-  next();
-});
-
-// ---- START SERVER --
-const PORT = process.env.PORT || 4900;
-const db = process.env.MONGODB_URI;
-mongoose.connect(db).then(()=> {
-    console.log('Database connection has been established successfully');
-    app.listen(PORT, ()=>{
-    console.log(`Server is running on the PORT: ${PORT}` );  
+ app.use((req, res) => {
+  res.status(404).json({
+    message: 'Route not found'  
+  })
 })
-}).catch((error=> {
-    console.error(`Error connecting to Database: ${error.message}`);
-}))
 
+app.use((err, req, res, next)=>{
+
+  if(err.name === 'TokenExpiredError') {
+            return res.status(401).json({
+                message: 'Session expired: please login to continue'
+            })
+        }
+
+        if(err.name === 'MulterError'){
+          return res.status(400).json({
+            message: err.message
+          })
+        }
+  console.log(err.message)
+  res.status(500).json({
+    message: 'something went wrong'
+  })
+})
+
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGODB_URI).then(()=>{
+    console.log('Connected to Database');
+  app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`)
+})  
+}).catch((error) => {
+    console.error('Error connecting to Database:', error.message);
+}); 
