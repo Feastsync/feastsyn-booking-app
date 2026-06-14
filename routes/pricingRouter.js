ccf2021d8574aaacb7ad4ae778a7c9470f3574d0
Taiwo-Adeoluwa <omogunloyetaiwo5@gmail.com>
2026-06-14 20:55:30 +0100

    committ

--- routes/pricingRouter.js
+++ routes/pricingRouter.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 router.post('/pricing', authentication, createPricing);
 router.put('/new-pricing/:pricingId', authentication, updatePricing);
-router.get('/all-pricing', getAllVendorPricing);
-router.get('/one-pricing/:pricingId', getOnePricing);
+router.get('/all-pricing', authentication, getAllVendorPricing);
+router.get('/one-pricing/:pricingId', authentication, getOnePricing);
 
 module.exports = router;
