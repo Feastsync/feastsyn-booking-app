e67857731e9d076db0c5b125f9b45c5f19035c56
Taiwo-Adeoluwa <omogunloyetaiwo5@gmail.com>
2026-06-15 19:45:29 +0100

    update function

--- routes/paymentRouter.js
+++ routes/paymentRouter.js
@@ -1,9 +1,9 @@
-const { initializePayment, verifyPayment, getAllPaymentByUser } = require('../controller/paymentController');
+const { initializePayment, getAllPaymentByUser, verifyWebhook } = require('../controller/paymentController');
 const { authentication } = require('../middlewares/auth');
 const router = require('express').Router();
 
 router.post('/initialize-payment/:vendorId/:bookingId', authentication, initializePayment);
-router.post('/verify-payment/:vendorId', authentication, verifyPayment);
+router.post('/webhook', verifyWebhook)
 router.get('/all-payments', authentication, getAllPaymentByUser);
 
 module.exports = router;
