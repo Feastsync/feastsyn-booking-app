8f4420b165dadccec48323e820b684ebdca86ed2
Taiwo-Adeoluwa <omogunloyetaiwo5@gmail.com>
2026-06-16 05:34:05 +0100

    onboarding steps

--- controller/vendorController.js
+++ controller/vendorController.js
@@ -167,7 +167,7 @@ exports.updateVendor = async (req, res) => {
       );
     }
 
-    const isOnboarded = nextOnboardingStep >= 4;
+    const isOnboarded = nextOnboardingStep >= 7;
 
     const uploadFile = async (file, resourceType = "image") => {
       const absolutePath = path.resolve(file.path);
