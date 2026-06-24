e6167fe750dff72c71712cbdca153a0ae47c90d6
Taiwo-Adeoluwa <omogunloyetaiwo5@gmail.com>
2026-06-24 20:11:28 +0100

    good

--- routes/calendarRouter.js
+++ routes/calendarRouter.js
@@ -3,5 +3,10 @@ const router = express.Router();
 const { getCalendar} = require('../controller/calendarController');
 
 router.get('/get-calendar/:vendorId', getCalendar);
-
+router.get('/test', (req, res) => {
+    console.log('CALENDAR TEST ROUTE HIT');
+    res.status(200).json({
+        message: 'Calendar route working'
+    });
+});
 module.exports = router;
